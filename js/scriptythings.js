d3dde4184e340d2b53f3d903ce015581f407e63a
Molly White <molly.white5@gmail.com>
2013-12-21 02:49:58 -0500

    Almost done :)

--- js/scriptythings.js
+++ js/scriptythings.js
@@ -1,2 +1,9 @@
 $(document).ready(function() {
-}
+	$('#nav').onePageNav({
+		currentClass: 'active',
+	    changeHash: false,
+	    scrollSpeed: 750,
+	    filter: '',
+	    easing: 'swing',
+	});
+});
