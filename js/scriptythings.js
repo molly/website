0c0eafe9ad8cd199a867f1f41d78c6460cbc87e5
Molly White <molly.white5@gmail.com>
2014-01-02 01:30:35 +0000

    Set changeHash to true, set min-height for top page

--- js/scriptythings.js
+++ js/scriptythings.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$("html").removeClass('no-js');
 	$('#nav').onePageNav({
 		currentClass: 'active',
-	    changeHash: false,
+	    changeHash: true,
 	    scrollSpeed: 750,
 	    filter: ':not(.external)',
 		easing: 'swing',
