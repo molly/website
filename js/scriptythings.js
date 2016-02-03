acd2a17bd15095399b3b605fc234a006259b0ac8
Molly White <molly.white5@gmail.com>
2016-02-03 15:48:42 -0500

    Redirect

--- js/scriptythings.js
+++ js/scriptythings.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-	$("html").removeClass('no-js');
-	$('#nav').onePageNav({
-		currentClass: 'active',
-	    changeHash: true,
-	    scrollSpeed: 750,
-	    filter: ':not(.external)',
-		easing: 'swing',
-	});
+  var grossReferer = /https?:\/\/www\.returnofkings\.com/
+  if (grossReferer.test(document.referrer)) {
+    window.location.href = "http://www.mollywhite.net/rok.html";
+  }
+  else {
+  	$("html").removeClass('no-js');
+  	$('#nav').onePageNav({
+  		currentClass: 'active',
+  	    changeHash: true,
+  	    scrollSpeed: 750,
+  	    filter: ':not(.external)',
+  		easing: 'swing',
+  	});
+  }
 });
