be65e03434d82d31edee96a15851f26fb08492e8
Molly White <molly.white5@gmail.com>
2016-02-03 15:08:10 -0500

    Cute things

---
+++ js/aww.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $("html").removeClass('no-js');
+  $aww = $("#aww");
+  $aww.empty();
+  $.get("http://api.reddit.com/r/aww/hot?limit=10", function(data) {
+    for (var i = data.data.children.length - 1; i >= 0; i--) {
+      var thumb;
+      try {
+        thumb = data.data.children[i].data.preview.images[0].source.url;
+      } catch(e) {
+        thumb = data.data.children[i].data.thumbnail;
+      }
+
+      if (thumb !== "self") {
+        $aww.append(
+          $("<div/>", {class: "aww-wrapper"}).append(
+            $("<a/>", {href: "https://www.reddit.com" + data.data.children[i].data.permalink}).append(
+              $("<img/>",
+                {class: "img-responsive",
+                 src: thumb,
+                 alt: data.data.children[i].data.title}
+              )
+            )
+          )
+          .append(
+            $("<span/>", {html: data.data.children[i].data.title})
+          )
+        );
+      };
+    };
+  });
+});
