c53d6737f54af185d4900122081fcf3cc4324756
Molly White <molly.white5@gmail.com>
2016-02-03 15:36:24 -0500

    Only show images

--- js/aww.js
+++ js/aww.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   $aww.empty();
   $.get("http://api.reddit.com/r/aww/hot?limit=10", function(data) {
     for (var i = data.data.children.length - 1; i >= 0; i--) {
+      console.log(data.data.children[i].data)
       var thumb;
       try {
         thumb = data.data.children[i].data.preview.images[0].source.url;
@@ -11,7 +12,7 @@ $(document).ready(function() {
         thumb = data.data.children[i].data.thumbnail;
       }
 
-      if (thumb !== "self") {
+      if (thumb !== "self" && data.data.children[i].data.post_hint === "image") {
         $aww.append('<div class="aww-wrapper"><a href="https://www.reddit.com' +
           data.data.children[i].data.permalink + '"><img class="img-responsive" src="' +
           thumb + '" alt="' + data.data.children[i].data.title +'"/></a><span>' +
